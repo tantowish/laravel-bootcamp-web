f06b1d78873c3540fd59bf2269697d45666cb5de
tanto <tantows001@gmail.com>
2023-09-18 22:30:27 +0700

    Slicing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/succes-checkout', function () {
+    return view('succes');
+});
+
